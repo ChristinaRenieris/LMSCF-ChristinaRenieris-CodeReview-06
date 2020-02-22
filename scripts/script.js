36345783f9e10431d16ef011f15fba0de1479d36
Christina <christinarenieris@gmail.com>
2020-02-22 12:55:48 +0100

    adding all classes

--- scripts/script.js
+++ scripts/script.js
@@ -30,7 +30,7 @@ var Locations = /** @class */ (function () {
     Locations.prototype.display = function () {
         for (var _i = 0, cardsL_1 = cardsL; _i < cardsL_1.length; _i++) {
             var i = cardsL_1[_i];
-            var places = "<div class=\"col-lg-3 col-md-5 col-sm-10 mb-3 d-flex alight-items-stretch\">\n                               <div class=\"card\">\n                                 <img class=\"card-img-top d-block d-xs-none d-sm-none  d-md-block\" src=\"" + this.img + "\" alt=\"Location image\">\n                                 <div class=\"card-body\">\n                                 <h4 class=\"card-title\">" + this.name + "</h4>\n                                 <p class=\"card-text\">\n                                 City : " + this.city + "<br>\n                                 Address : " + this.address + "<br>\n                                 Zip-Code : " + this.zipCode + "<br>\n                                 </p>\n                                 </div>\n                                 </div>\n                                 </div>";
+            var places = "<div class=\"col-lg-3 col-md-5 col-sm-10 mb-3 d-flex alight-items-stretch\">\n                               <div class=\"card w-100\">\n                                 <img class=\"card-img-top d-none d-md-block  d-lg-block  d-xl-block\" src=\"" + this.img + "\" alt=\"Location image\">\n                                 <div class=\"card-body\">\n                                 <h4 class=\"card-title\">" + this.name + "</h4>\n                                 <p class=\"card-text\">\n                                 City : " + this.city + "<br>\n                                 Address : " + this.address + "<br>\n                                 Zip-Code : " + this.zipCode + "<br>\n                                 </p>\n                                 </div>\n                                 </div>\n                                 </div>";
         }
         document.getElementById("locationCards").innerHTML += places;
     };
@@ -48,18 +48,43 @@ var Restaurants = /** @class */ (function (_super) {
         _this.type = type;
         _this.webAddress = webAddress;
         cardsR.push(_this);
-        _this.showR();
+        _this.displayR();
         return _this;
     }
-    Restaurants.prototype.showR = function () {
+    Restaurants.prototype.displayR = function () {
         for (var _i = 0, cardsR_1 = cardsR; _i < cardsR_1.length; _i++) {
             var i = cardsR_1[_i];
-            var diners = "<div class=\"col-lg-2 col-md-5 col-sm-10 mb-3 d-flex alight-items-stretch\">\n                               <div class=\"card\">\n                                 <img class=\"card-img-top d-block d-sm-none d-md-block\" src=\"" + this.img + "\" alt=\"Restaurant image\">\n                                 <div class=\"card-body\">\n                                 <h4 class=\"card-title\">" + this.name + "</h4>\n                                 <p class=\"card-text\">\n                                 City : " + this.city + "<br>\n                                 Address : " + this.address + "<br>\n                                 Zip-Code : " + this.zipCode + "<br>\n                                 Tel. : " + this.phoneNumber + "<br>\n                                 Type : " + this.type + "<br>\n                                 Website : " + this.webAddress + "<br>\n                                 </p>\n                                 </div>\n                                 </div>\n                                 </div>";
+            var diners = "<div class=\"col-lg-3 col-md-5 col-sm-10 mb-3 d-flex alight-items-stretch\">\n                               <div class=\"card w-100\">\n                                 <img class=\"card-img-top d-none d-md-block  d-lg-block  d-xl-block\" src=\"" + this.img + "\" alt=\"Restaurant image\">\n                                 <div class=\"card-body\">\n                                 <h4 class=\"card-title\">" + this.name + "</h4>\n                                 <p class=\"card-text\">\n                                 City : " + this.city + "<br>\n                                 Address : " + this.address + "<br>\n                                 Zip-Code : " + this.zipCode + "<br>\n                                 Tel. : " + this.phoneNumber + "<br>\n                                 Type : " + this.type + "<br>\n                                 Website : " + this.webAddress + "<br>\n                                 </p>\n                                 </div>\n                                 </div>\n                                 </div>";
             console.table(this);
         }
         document.getElementById("restaurantCards").innerHTML += diners;
     };
     return Restaurants;
+}(Locations));
+var cardsE = [];
+var Events = /** @class */ (function (_super) {
+    __extends(Events, _super);
+    function Events(name, city, zipCode, address, img, eventDate, eventTime, ticket) {
+        var _this = _super.call(this, name, city, zipCode, address, img) || this;
+        _this.eventDate = "";
+        _this.eventTime = "";
+        _this.ticket = "";
+        _this.eventDate = eventDate;
+        _this.eventTime = eventTime;
+        _this.ticket = ticket;
+        cardsE.push(_this);
+        _this.displayE();
+        return _this;
+    }
+    Events.prototype.displayE = function () {
+        for (var _i = 0, cardsR_2 = cardsR; _i < cardsR_2.length; _i++) {
+            var i = cardsR_2[_i];
+            var occasions = "<div class=\"col-lg-3 col-md-5 col-sm-10 mb-3 d-flex alight-items-stretch\">\n                               <div class=\"card w-100\">\n                                 <img class=\"card-img-top d-none d-md-block  d-lg-block  d-xl-block\" src=\"" + this.img + "\" alt=\"Restaurant image\">\n                                 <div class=\"card-body\">\n                                 <h4 class=\"card-title\">" + this.name + "</h4>\n                                 <p class=\"card-text\">\n                                 City : " + this.city + "<br>\n                                 Address : " + this.address + "<br>\n                                 Zip-Code : " + this.zipCode + "<br>\n                                 Date : " + this.eventDate + "<br>\n                                 Time : " + this.eventTime + "<br>\n                                 Ticket : " + this.ticket + " &euro;<br>\n                                 </p>\n                                 </div>\n                                 </div>\n                                 </div>";
+            console.table(this);
+        }
+        document.getElementById("eventCards").innerHTML += occasions;
+    };
+    return Events;
 }(Locations));
 //Locations 
 var CharlesChurch = new Locations("Karls Kirche", "Vienna", "1010", "Karlsplatz 1", "img/charles.jpg");
@@ -71,3 +96,8 @@ var SwingKitchen = new Restaurants("Swing Kitchen", "Vienna", "1040", "Operngass
 var Gastrologik = new Restaurants("Gastrologik", "Stockholm", "114 51", "14 Artillerigatan", "img/Gastrologik.jpg", "4686623060", "Swedish/Scandinavian", "www.gastrologik.se");
 var TheRealGreek = new Restaurants("The Real Greek", "London", "W1F 8SJ", "50 Berwick Street", "img/realGreek.jpg", "442072876805", "Greek/Mediterranean", "www.therealgreek.com");
 var ConGracia = new Restaurants("Con Gracia", "Barcelona", "08012", "Martinez de la Rosa 8", "img/congracia.jpg", "34932380201", "Spanish/Catalan", "www.congracia.es/es/inicio");
+//Events
+var PrincessNokia = new Events("Princess Nokia Live", "Vienna", "1030", "Baumgasse 80", "img/nokia.jpg", "5 March 2020", "19:00", "30");
+var HouseOfRock = new Events("House of Rock", "Stockholm", "112 38", "Kungsgatan 29", "img/rock.jpg", "19 May 2020", "18:00", "100");
+var Diving = new Events("Diving Championship", "London", "E20 2ST", "Olympic Park", "img/diving.jpg", "27-29 March 2020", "9:00", "15");
+var Choir = new Events("Vienna Boys Choir", "Barcelona", "08002", "C/De St Pere 11", "img/choir.jpg", "22 February 2020", "20:00", "45");
--- scripts/script.ts
+++ scripts/script.ts
@@ -20,8 +20,8 @@ class Locations{
     display() {
         for( let i of cardsL){
             var places =  `<div class="col-lg-3 col-md-5 col-sm-10 mb-3 d-flex alight-items-stretch">
-                               <div class="card">
-                                 <img class="card-img-top d-block d-xs-none d-sm-none  d-md-block" src="${this.img}" alt="Location image">
+                               <div class="card w-100">
+                                 <img class="card-img-top d-none d-md-block  d-lg-block  d-xl-block" src="${this.img}" alt="Location image">
                                  <div class="card-body">
                                  <h4 class="card-title">${this.name}</h4>
                                  <p class="card-text">
@@ -52,14 +52,14 @@ class Restaurants extends Locations{
         this.type = type;
         this.webAddress = webAddress;
         cardsR.push(this);
-        this.showR();
+        this.displayR();
     }
 
-    showR(){
+    displayR(){
         for( let i of cardsR){
-            var  diners=  `<div class="col-lg-2 col-md-5 col-sm-10 mb-3 d-flex alight-items-stretch">
-                               <div class="card">
-                                 <img class="card-img-top d-block d-sm-none d-md-block" src="${this.img}" alt="Restaurant image">
+            var  diners=  `<div class="col-lg-3 col-md-5 col-sm-10 mb-3 d-flex alight-items-stretch">
+                               <div class="card w-100">
+                                 <img class="card-img-top d-none d-md-block  d-lg-block  d-xl-block" src="${this.img}" alt="Restaurant image">
                                  <div class="card-body">
                                  <h4 class="card-title">${this.name}</h4>
                                  <p class="card-text">
@@ -81,6 +81,48 @@ class Restaurants extends Locations{
 }
 
 
+var cardsE: Array <{}> = [];
+
+class Events extends Locations{
+    eventDate = "";
+    eventTime = "";
+    ticket = "";
+
+    constructor(name, city, zipCode, address, img, eventDate, eventTime, ticket){
+        super(name, city, zipCode, address, img);
+        this.eventDate = eventDate;
+        this.eventTime = eventTime;
+        this.ticket = ticket;
+        cardsE.push(this);
+        this.displayE();
+    }
+
+    displayE(){
+        for( let i of cardsR){
+            var occasions =  `<div class="col-lg-3 col-md-5 col-sm-10 mb-3 d-flex alight-items-stretch">
+                               <div class="card w-100">
+                                 <img class="card-img-top d-none d-md-block  d-lg-block  d-xl-block" src="${this.img}" alt="Restaurant image">
+                                 <div class="card-body">
+                                 <h4 class="card-title">${this.name}</h4>
+                                 <p class="card-text">
+                                 City : ${this.city}<br>
+                                 Address : ${this.address}<br>
+                                 Zip-Code : ${this.zipCode}<br>
+                                 Date : ${this.eventDate}<br>
+                                 Time : ${this.eventTime}<br>
+                                 Ticket : ${this.ticket} &euro;<br>
+                                 </p>
+                                 </div>
+                                 </div>
+                                 </div>`;
+                                 console.table(this);
+        }
+
+        document.getElementById("eventCards").innerHTML += occasions ;
+    }
+}
+
+
 
 
 
@@ -96,3 +138,10 @@ let SwingKitchen = new Restaurants("Swing Kitchen", "Vienna", "1040", "Operngass
 let Gastrologik = new Restaurants("Gastrologik", "Stockholm", "114 51", "14 Artillerigatan", "img/Gastrologik.jpg", "4686623060", "Swedish/Scandinavian", "www.gastrologik.se");
 let TheRealGreek = new Restaurants("The Real Greek", "London", "W1F 8SJ", "50 Berwick Street", "img/realGreek.jpg", "442072876805", "Greek/Mediterranean", "www.therealgreek.com");
 let ConGracia = new Restaurants("Con Gracia", "Barcelona", "08012", "Martinez de la Rosa 8", "img/congracia.jpg", "34932380201", "Spanish/Catalan", "www.congracia.es/es/inicio");
+
+//Events
+
+let PrincessNokia = new Events("Princess Nokia Live", "Vienna", "1030", "Baumgasse 80", "img/nokia.jpg", "5 March 2020", "19:00", "30");
+let HouseOfRock = new Events("House of Rock", "Stockholm", "112 38", "Kungsgatan 29", "img/rock.jpg", "19 May 2020", "18:00", "100");
+let Diving = new Events("Diving Championship", "London", "E20 2ST", "Olympic Park", "img/diving.jpg", "27-29 March 2020", "9:00", "15");
+let Choir = new Events("Vienna Boys Choir", "Barcelona", "08002", "C/De St Pere 11", "img/choir.jpg", "22 February 2020", "20:00", "45");
